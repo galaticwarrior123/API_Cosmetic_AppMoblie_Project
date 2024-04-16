6169614ca739b66abbf6bdace0c1c8311e797daf
galaticwarrior123 <trongnguyenphuc22082003@gmail.com>
2024-04-16 09:15:06 +0700

    update link api

--- src/main/java/com/example/cosmetic_springboot_api/Controller/BrandController.java
+++ src/main/java/com/example/cosmetic_springboot_api/Controller/BrandController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/brands")
+@RequestMapping("/api/brand/")
 public class BrandController {
     private final IBrandService brandService;
 
